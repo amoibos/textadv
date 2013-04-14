839936a3f04a8b07c7361b38ddb8a05d25636fb4
kevindecker <keevin.decker@gmail.com>
2013-04-14 13:59:47 +0300

    Update main.py
    outsourced the game class

--- main.py
+++ main.py
@@ -3,177 +3,138 @@ import random
 import itertools
 import os
 import platform
+import pickle
+import game
+import sys
+
+def clear():
+    if platform.system() == "Linux":
+        os.system("clear")
+    elif platform.system() == "Windows":
+        os.system("cls")
 
 
-random.seed()
-
-
-def main():
-    def clear():
-        if platform.system() == "Linux":
-            os.system("clear")
-        elif platform.system() == "Windows":
-            os.system("cls")
-
-    class NewGame():
-        def __init__(self):
-            self.player = character.NewCharacter("Dude",
-            False, 1, 10, 100, 100, 15, 100)
-
-            self.opponent = character.NewCharacter(character.random_name(),
-            True, 1, 10, 120, 120, 15, 50)
-
-            self.round = 1
-            self.state = "running"  # running, over, new
-
-        #  check if a character has been defeated, initiate new round
-        def check_defeated(self):
-            if self.player.vitality <= 0:
-                print("{0} has been defeated by {1}".format(self.player.name,
-                self.opponent.name))
-                self.state = "over"
-                return True
-
-            elif self.opponent.vitality <= 0:
-                self.round += 1
-                print("{0} has been defeated by {1}, prepare for round {2}"
-                    .format(self.opponent.name, self.player.name, self.round))
-                self.player.improve()
-                self.opponent.improve()
-                self.opponent.name = character.random_name()
-                return True
-
-        def info(self):
-            print("-------------------------------------------------------")
-            print("Round: {0}, Game state: {1}, Player: {2}, Opponent: {3}"
-                .format(self.round, self.state, self.player.name,
-                self.opponent.name))
-            print("-------------------------------------------------------")
-
-        def print_character_status(self):
-            print("""
-                Name: {0} \t\t Name: {1}
-                Vitality: {2}/{3} \t Vitality: {4}/{5} \n""".format(
-                    self.player.name, self.opponent.name, self.player.vitality,
-                    self.player.max_vitality, self.opponent.vitality,
-                    self.opponent.max_vitality))
-
-        #  index is the key of the attacks in the list of dicts "attacks"
-        def print_attack_status(self, character):
-            for index, attack in character.attacks.items():
-                print("\t\t{0}: {1} cooldown: {2}/{3}".format(
-                    index, attack.name, attack.cooldown_counter,
-                    attack.cooldown))
-
-        #  write all ready skills in "available" and randomly pick on of them
-        def random_attack(self):
-            available = []
-            for index, attack in self.opponent.attacks.items():
-                if attack.cooldown_counter == attack.cooldown:
-                    available.append(index)
-            return random.choice(available)
-
-        # ask player for attack input, check input, check cooldowns
-        def attack_selection(self):
-            while True:
-                while True:
-                    try:
-                        selection = int(input("Attack:"))
-                        break
-                    except ValueError:
-                        print("Wrong input, enter a number")
-                if self.player.select_attack(selection) is True:
-                    return selection
-                else:
-                    continue
-
-        def set_attack_cooldown(self, character):
-            # setting cooldowns
-            for obj in character.attacks.values():
-                if obj.cooldown_counter < obj.cooldown:
-                    obj.cooldown_counter += 1
-                else:
-                    continue
+def main():               
     
-    def battle(game):
+    def run(Game):
         while True:
-            if game.state == "over":
+            if Game.state == "over":
                 break
             else:
                 pass
 
-            game.info()
-            game.print_character_status()
-            game.print_attack_status(game.player)
-
-            #  PLAYER SCENE
-            #  player attack input
-            selection = game.attack_selection()
-
-            #  set selected skill on cooldown
-            game.player.attacks[selection].cooldown_counter -= \
-            game.player.attacks[selection].cooldown + 1
-
-            #  damage caluclation
-            damage = ((game.player.attacks[selection].damage_mod +
-                game.player.base_damage) - game.opponent.defense)
-
-            game.opponent.vitality -= damage
-
-            #  text output
-            print("You attacked with a {0} and dealed {1} damage".format(
-                game.player.attacks[selection].name, damage))
-
-            #  set cooldowns
-            game.set_attack_cooldown(game.player)
-
-            #  if check_defeated is True game.state == "over"
-            #  means break out of "battle loop"
-            if game.check_defeated() is True:
+            print("1 Battle")
+            print("2 Character menu")
+            print("3 Save Game")
+            print("4 Exit Game")
+            while True:
+                try:
+                    Game_menu = int(input("Game menu:"))
+                    break
+                except ValueError:
+                    print("Enter a number")
+            if Game_menu == 1:
+                pass
+            elif Game_menu == 2:
+                Game.character_menu()
                 continue
-
-            #  OPPONENT SCENE
-            #  select random attack
-            attack = game.random_attack()
-
-            #  set selected skill on cooldown
-            game.opponent.attacks[attack].cooldown_counter -= \
-                game.opponent.attacks[attack].cooldown + 1
-
-            #  damage calculation
-            damage = (
-                (game.opponent.attacks[attack].damage_mod +
-                    game.opponent.base_damage) - game.player.defense)
-
-            game.player.vitality -= damage
-
-            #  text output
-            print("{0} attacked with {1} and dealed {2} damage".format(
-                game.opponent.name, game.opponent.attacks[attack].name,
-                damage))
-
-            #  set cooldowns
-            game.set_attack_cooldown(game.opponent)
-
-            if game.check_defeated() is True:
+            elif Game_menu == 3:
+                try:
+                    with open("data.pickle", "wb") as f:
+                        pickle.dump(Game, f, pickle.HIGHEST_PROTOCOL)
+                        print("Game saved successfull")
+                except:
+                    print("Problem with saving game")  # need to improve this
                 continue
+            elif Game_menu == 4:
+                sys.exit(0)
 
-            x = input("hit enter to continue")
-            clear()
-        print("game over: 1")
+
+            while True:
+                Game.info()
+                print("\tName: {0}\t\tName: {1}".format(Game.player.name,
+                    Game.opponent.name))
+                print("\tVitality: {0}/{1}\tVitality:{2}/{3}\n".format(
+                    Game.player.vitality, Game.player.max_vitality,
+                    Game.opponent.vitality, Game.opponent.max_vitality))
+
+                Game.player.print_attack_status()
+
+                #  PLAYER SCENE
+                #  player attack input
+                selection = Game.attack_selection()
+
+                #  set selected skill on cooldown
+                Game.player.set_attack_cooldown(selection)
+
+                #  damage caluclation
+                damage = Game.damage_calc(selection)
+                Game.opponent.vitality -= damage
+
+                #  text output
+                print("You attacked with a {0} and dealed {1} damage".format(
+                    Game.player.attacks[selection].name, damage))
+
+                #  reduce cooldown of each attack
+                Game.player.reduce_attack_cooldown()
+
+                #  if ("opponent") defeated is True Game.state == ""
+                #  means break out of "battle loop"
+                if Game.defeated() is True:
+                    break
+
+                #  OPPONENT SCENE
+                #  select random attack
+                attack = Game.opponent.random_attack()
+
+                #  set selected skill on cooldown
+                Game.opponent.set_attack_cooldown(attack)
+
+                #  damage calculation
+                damage = (
+                    (Game.opponent.attacks[attack].damage_mod +
+                        Game.opponent.base_damage) - Game.player.defense)
+
+                Game.player.vitality -= damage
+
+                #  text output
+                print("{0} attacked with {1} and dealed {2} damage".format(
+                    Game.opponent.name, Game.opponent.attacks[attack].name,
+                    damage))
+
+                #  set cooldowns
+                Game.opponent.reduce_attack_cooldown()
+
+                if Game.defeated() is True:
+                    break
+
+                x = input("hit enter to continue")
+                clear()
+        print("Game over: 1")
 
     while True:
 
         print("""
-            1 New game
-            2 Load game
-            3 Exit""")
+            1 New Game
+            2 Continue Game
+            4 Exit""")
+        while True:
+            try:
+                menu_selection = int(input("Menu:"))
+                break
+            except ValueError:
+                print("Enter a number")
 
-        menu_selection = int(input("Menu:"))
         if menu_selection == 1:
             clear()
-            game_session = NewGame()
-            battle(game_session)
+            game_session = game.Game()
+            run(game_session)
+        elif menu_selection == 2:
+            with open("data.pickle", "rb") as f:
+                game_data = pickle.load(f)
+            run(game_data)
+        elif menu_selection ==3:
+            print("There is no 3!")
 
 
         else:
