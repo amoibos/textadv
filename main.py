644fe2e898ded39b2f9ce1722361af48f5e10133
kevindecker <keevin.decker@gmail.com>
2013-04-17 23:46:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,76 @@ import pickle
 import game
 import sys
 
+class Menu():
+    def __init__(self, mID, mlabel, mtype):
+        self.mID = mID  # Number displayed besides "mlabel"
+        self.mlabel = mlabel  # for example "New game"
+        self.mtype = mtype  # defines the menu category "main", "game"...
+
+    def print_menu_entry(self):
+        print("{0} {1}".format(self.mID, self.mlabel))
+
+    def is_main(self):
+        if self.mtype == "main":
+            return True
+        else:
+            return False
+
+    def is_battle(self):
+        if self.mtype == "battle":
+            return True
+        else:
+            return False
+
+    def command_main(self):
+        if self.is_main():
+            if self.mID == 1:
+                clear()
+                run_game(game.CreateGame())
+            elif self.mID == 2:
+                with open("data.pickle", "rb") as f:
+                    game_data = pickle.load(f)
+                run_game(game_data)
+            elif self.mID == 3:
+                sys.exit(0)
+            else:
+                print("Wrong number")
+    def command_battle(self, Game):
+        if self.is_battle():
+            if self.mID == 1:
+                clear()
+                return True
+
+            elif self.mID == 2:
+                clear()
+                Game.character_menu()
+            elif self.mID == 3:
+                try:
+                    with open("data.pickle", "wb") as f:
+                        pickle.dump(Game, f, pickle.HIGHEST_PROTOCOL)
+                        print("Game saved successfull")
+                        os.system("pause")
+                        clear()
+                except:
+                    print("Problem with saving game")  # need to improve this
+            elif self.mID == 4:
+                sys.exit(0)
+
+
+#  main menu
+m_new_game = Menu(1, "New game", "main")
+m_load_game = Menu(2, "Load game", "main")
+m_exit_game = Menu(3, "Exit", "main")
+m_main_menu = {1:m_new_game, 2:m_load_game, 3:m_exit_game}
+
+# battle menu
+m_battle = Menu(1, "Start battle", "battle")
+m_character = Menu(2, "Character menu", "battle")
+m_save_game = Menu(3, "Save game", "battle")
+m_exit_game = Menu(4, "Exit game", "battle")
+
+m_battle_menu = {1:m_battle, 2:m_character, 3:m_save_game, 4:m_exit_game}
+
 
 # clear display win/linux
 def clear():
@@ -13,6 +83,11 @@ def clear():
         os.system("clear")
     elif platform.system() == "Windows":
         os.system("cls")
+
+def pause():
+    input("Hit any key to continue")
+
+
 
 #  asks user for input, menu_section is the "input label"
 def user_input(menu_section):
@@ -33,71 +108,39 @@ def run_game(Game):
 
         if Game.state == "over":
             break
-        else:
-            pass
+        while True:
+            for obj in m_battle_menu.values():
+                obj.print_menu_entry()
+            game_menu = int(user_input("Game menu:"))
+            if m_battle_menu[game_menu].command_battle(Game):
+                break
 
-        print("1 Battle")
-        print("2 Character menu")
-        print("3 Save Game")
-        print("4 Exit Game")
-
-        game_menu = int(user_input("Game menu:"))
-        if game_menu == 1:  # start battle
-            clear()
-            pass
-        elif game_menu == 2:  # character menu
-            clear()
-            Game.character_menu()
-            continue
-        elif game_menu == 3:  # save game
-            try:
-                with open("data.pickle", "wb") as f:
-                    pickle.dump(Game, f, pickle.HIGHEST_PROTOCOL)
-                    print("Game saved successfull")
-                    x = input("Hit enter to continue")
-                    clear()
-            except:
-                print("Problem with saving game")  # need to improve this
-            continue
-        elif Game_menu == 4:
-            sys.exit(0)
 
         while True:  # battle sequence
             Game.info()
-            print(Game.player.character_status())
-            print(Game.opponent.character_status())
+            print(Game.player.print_character_status())
+            print(Game.opponent.print_character_status())
             print("\n")
-            
-            if Game.battle_sequence() is False:
-                break
 
-            x = input("hit enter to continue")
+            if not Game.battle_sequence():
+                break
+            pause()
             clear()
-    print("Game over: 1")
+    print("\t---GAME OVER---")
+    pause()
+
 
 # main menu, creates or loads a game instance
 def main():
     while True:
-        clear()
-        print("""
-            1 New Game
-            2 Continue Game
-            4 Exit""")
+        for obj in m_main_menu.values():
+            obj.print_menu_entry()
 
         menu_selection = int(user_input("Main menu:"))
-
-        if menu_selection == 1:
-            clear()
-            game_session = game.CreateGame()
-            run_game(game_session)
-        elif menu_selection == 2:
-            with open("data.pickle", "rb") as f:
-                game_data = pickle.load(f)
-            run_game(game_data)
-        elif menu_selection == 3:
-            print("3...")
-        else:
-            print("exit ctrl + c")
+        try:
+            m_main_menu[menu_selection].command_main()
+        except KeyError:
+            print("Select a valid number")
 
 if __name__ == "__main__":
     main()
